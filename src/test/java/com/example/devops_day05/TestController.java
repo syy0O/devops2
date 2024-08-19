dd48480814f9ff5d7f16277f9b0a4cc12c1f693f
syy0O <songie05@naver.com>
2024-08-19 12:26:17 +0900

    push1

---
+++ src/test/java/com/example/devops_day05/TestController.java
@@ -0,0 +1,12 @@
+package com.example.devops_day05;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "test 입니다!";
+    }
+}
