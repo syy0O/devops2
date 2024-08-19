fe17c88e679be854371f883bfca66a9c4efef6ed
syy0O <songie05@naver.com>
2024-08-19 12:35:37 +0900

    push3

--- src/test/java/com/example/devops_day05/TestController.java
+++ src/test/java/com/example/devops_day05/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "test 입니다!";
+        return "test다!";
     }
 }
